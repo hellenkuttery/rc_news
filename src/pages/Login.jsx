1d454edacd8987951ddf15f97e8b5fbc9e90d8b7
hellenkuttery <helen@clarusway.com>
2023-09-23 00:50:16 +0300

    add login changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -130,7 +130,7 @@ export default function SignInSide() {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link href="#" variant="body2">
+                  <Link href="#" variant="body2">s
                     Forgot password?
                   </Link>
                 </Grid>
