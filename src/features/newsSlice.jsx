6c3b74e5dc03e4603b1d0989bc559cc5a647b652
hellenkuttery <helen@clarusway.com>
2023-09-22 18:04:03 +0300

    add changes

--- src/features/newsSlice.jsx
+++ src/features/newsSlice.jsx
@@ -1,29 +1,44 @@
-import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
-import axios from "axios"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+// import { getNews } from "./newsSlice";
 const initialState = {
-news:[],
-}
-export const getNews=createAsyncThunk(
-  "getNewsFunc",  //action type name
-  async()=>{
-
-// const API_KEY=a033b4ca75b345d6959e331522c91ece
-const url="https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}"
-const {data}=await axios(url)
-console.log(data);
+  news: [],
+  loading:true,
+  error:false
+};
+export const getNews = createAsyncThunk(
+  "getNewsFunc", //action type name
+  async () => {
+    const API_KEY = "a033b4ca75b345d6959e331522c91ece";
+    const url = "https://newsapi.org/v2/top-headlines?country=tr&apiKey=a033b4ca75b345d6959e331522c91ece";
+    const { data } = await axios(url);
+    console.log(data);
   }
-)
+);
 
 const newsSlice = createSlice({
   name: "news",
   initialState,
   reducers: {
-    clearNews:(state)=>{
-      state.news=[]
-    }
-  }
+    clearNews: (state) => {
+      state.news = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(
+      (getNews.pending,
+      (state) => {
+        state.loading = true;
+      }))
+      .addCase((getNews.fulfilled,(state,action)=>{
+        state.news=action.payload
+        state.loading = false;
+      })
+      )
+    
+  },
 });
 
-export const {} = newsSlice.actions
+export const { clearNews } = newsSlice.actions;
 
-export default newsSlice.reducer
+export default newsSlice.reducer;
